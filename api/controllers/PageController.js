1e4f1b33eba5c19caa2f2b2b114ac39a61f3fcc2
Guy Sopher <GuySopher@gmail.com>
2016-01-16 13:32:23 +0200

    few moew style pages

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -14,6 +14,13 @@ module.exports = {
         {route: 'position', name: 'Positioning'},
         {route: 'display', name: 'Display'},
         {route: 'color', name: 'Colors'},
+        {route: 'sizing', name: 'Sizing'},
+        {route: 'box-model', name: 'Box Model'},
+        {route: 'border', name: 'Borders'},
+        {route: 'overflow', name: 'Overflow'},
+        {route: 'font', name: 'Font Styling'},
+        {route: 'text', name: 'Text Styling'},
+        {route: 'float', name: 'Floats'},
     ];
 
 
