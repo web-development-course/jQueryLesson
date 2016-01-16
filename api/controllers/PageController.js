5476cf364304cec845ddca804102f6e2573f0d9c
Guy Sopher <GuySopher@gmail.com>
2016-01-16 13:33:23 +0200

    change dropdown order

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -11,15 +11,15 @@ module.exports = {
     var param = req.allParams();
 
     var navs = [
-        {route: 'position', name: 'Positioning'},
-        {route: 'display', name: 'Display'},
+        {route: 'text', name: 'Text Styling'},
+        {route: 'font', name: 'Font Styling'},
         {route: 'color', name: 'Colors'},
+        {route: 'display', name: 'Display'},
+        {route: 'position', name: 'Positioning'},
         {route: 'sizing', name: 'Sizing'},
         {route: 'box-model', name: 'Box Model'},
         {route: 'border', name: 'Borders'},
         {route: 'overflow', name: 'Overflow'},
-        {route: 'font', name: 'Font Styling'},
-        {route: 'text', name: 'Text Styling'},
         {route: 'float', name: 'Floats'},
     ];
 
