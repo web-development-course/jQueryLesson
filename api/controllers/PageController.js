76cb029c6a1a65ab2071bafd485231615c10b140
Guy Sopher <GuySopher@gmail.com>
2016-02-27 16:44:53 +0200

    complete all lessons

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -12,13 +12,15 @@ module.exports = {
     var param = req.allParams();
 
     var navs = [
+        {route: 'click', name: 'Click Event'},
+        {route: 'set', name: 'Set / Get Content'},
+        {route: 'elements', name: 'Creating Elements'},
         {route: 'show', name: 'Show / Hide'},
         {route: 'fade', name: 'Fade In / Fade Out'},
         {route: 'slide', name: 'Slide Down / Slide Up'},
         {route: 'classes', name: 'CSS Classes'},
         {route: 'styles', name: 'CSS Styling'},
         {route: 'dimensions', name: 'Dimensions'},
-        {route: 'elements', name: 'Creating Elements'},
     ];
 
 
